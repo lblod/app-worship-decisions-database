c5d8b9aa987210ac52ea9c67bb53ff7c7adb56b8
Dries Hugaerts <dries.hugaerts@live.be>
2021-10-28 16:51:43 +0200

    added min. dispatcher and resource configuration + added cache and file services to the stack.

--- config/dispatcher/dispatcher.ex
+++ config/dispatcher/dispatcher.ex
@@ -1,23 +1,98 @@
 defmodule Dispatcher do
   use Matcher
   define_accept_types [
+    any: ["*/*"],
     html: [ "text/html", "application/xhtml+html" ],
     json: [ "application/json", "application/vnd.api+json" ]
   ]
 
-  @any %{}
+  @any %{ accept: %{ any: true } }
   @json %{ accept: %{ json: true } }
   @html %{ accept: %{ html: true } }
 
-  # In order to forward the 'themes' resource to the
-  # resource service, use the following forward rule:
-  #
-  # match "/themes/*path", @json do
-  #   Proxy.forward conn, path, "http://resource/themes/"
-  # end
-  #
-  # Run `docker-compose restart dispatcher` after updating
-  # this file.
+  ###############################################################
+  # General/Shared
+  ###############################################################
+
+  get "/bestuurseenheden/*path", @json do
+    Proxy.forward conn, path, "http://cache/bestuurseenheden/"
+  end
+
+  get "/werkingsgebieden/*path", @json do
+    Proxy.forward conn, path, "http://cache/werkingsgebieden/"
+  end
+
+  get "/bestuurseenheid-classificatie-codes/*path", @json do
+    Proxy.forward conn, path, "http://cache/bestuurseenheid-classificatie-codes/"
+  end
+
+  get "/bestuursorganen/*path", @json do
+    Proxy.forward conn, path, "http://cache/bestuursorganen/"
+  end
+
+  get "/bestuursorgaan-classificatie-codes/*path", @json do
+    Proxy.forward conn, path, "http://cache/bestuursorgaan-classificatie-codes/"
+  end
+
+  get "/files/*path", @json do
+    Proxy.forward conn, path, "http://resource/files/"
+  end
+
+  get "/files/:id/download", @any do
+    Proxy.forward conn, [], "http://file/files/" <> id <> "/download"
+  end
+
+  #################################################################
+  # Submissions
+  #################################################################
+
+  get "/remote-urls/*path", @json do
+    Proxy.forward conn, path, "http://cache/remote-urls/"
+  end
+
+  get "/inzendingen-voor-toezicht/*path", @json do
+    Proxy.forward conn, path, "http://cache/inzendingen-voor-toezicht/"
+  end
+
+  get "/submissions/*path", @json do
+    Proxy.forward conn, path, "http://cache/submissions/"
+  end
+
+  get "/authenticity-types/*path", @json do
+    Proxy.forward conn, path, "http://cache/authenticity-types/"
+  end
+
+  get "/tax-types/*path", @json do
+    Proxy.forward conn, path, "http://cache/tax-types/"
+  end
+
+  get "/chart-of-accounts/*path", @json do
+    Proxy.forward conn, path, "http://cache/chart-of-accounts/"
+  end
+
+  get "/submission-document-statuses/*path", @json do
+    Proxy.forward conn, path, "http://cache/submission-document-statuses/"
+  end
+
+  get "/submission-documents/*path", @json do
+    Proxy.forward conn, path, "http://cache/submission-documents/"
+  end
+
+  get "/form-data/*path", @json do
+    Proxy.forward conn, path, "http://cache/form-data/"
+  end
+
+  get "/concept-schemes/*path", @json do
+    Proxy.forward conn, path, "http://cache/concept-schemes/"
+  end
+
+  get "/concepts/*path", @json do
+    Proxy.forward conn, path, "http://cache/concepts/"
+  end
+
+  #################################################################
+  # Fallback
+  #################################################################
 
   match "/*_", %{ last_call: true } do
     send_resp( conn, 404, "Route not found.  See config/dispatcher.ex" )
--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -1,4 +1,18 @@
 (in-package :mu-cl-resources)
 
-;; reading in the domain.json
-(read-domain-file "domain.json")
+(defparameter *cache-count-queries* nil)
+(defparameter *supply-cache-headers-p* t
+  "when non-nil, cache headers are supplied.  this works together with mu-cache.")
+(setf *cache-model-properties-p* t)
+
+(defparameter *include-count-in-paginated-responses* t
+  "when non-nil, all paginated listings will contain the number
+   of responses in the result object's meta.")
+(defparameter *max-group-sorted-properties* nil)
+(defparameter sparql:*experimental-no-application-graph-for-sudo-select-queries* t)
+
+(read-domain-file "slave-besluit-domain.lisp")
+(read-domain-file "slave-files-domain.lisp")
+(read-domain-file "slave-submissions-domain.lisp")
+(read-domain-file "slave-toezicht-domain.lisp")
+(read-domain-file "slave-users-domain.lisp")
