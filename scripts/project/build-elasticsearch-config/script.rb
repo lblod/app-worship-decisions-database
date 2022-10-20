005eff7d9591ba36dc6722c07f4c1b54b065974e
felix <ruiz.felixruiz@gmail.com>
2022-10-20 11:26:08 +0200

    updated mu-script

--- scripts/project/build-elasticsearch-config/script.rb
+++ scripts/project/build-elasticsearch-config/script.rb
@@ -20,12 +20,6 @@ def query()
             PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 
             SELECT DISTINCT ?s ?uuid ?label WHERE {
-              VALUES ?s {
-                    <http://data.lblod.info/id/bestuurseenheden/319016d52cb54b416721b0c5fc74f211fdd4dd576d13a34aa9210759401dc7f2>
-                    <http://data.lblod.info/id/besturenVanDeEredienst/ca33dfe510518167b156bfca7f7c56b6>
-                    <http://data.lblod.info/id/bestuurseenheden/141d9d6b-54af-4d17-b313-8d1c30bc3f5b>
-                    <http://data.lblod.info/id/representatieveOrganen/b4d22cde910a7b58b6e4d9c1d3b15fbb>
-               }
               ?s a besluit:Bestuurseenheid;
                 skos:prefLabel ?label;
                 mu:uuid ?uuid.
@@ -45,13 +39,13 @@ def get_eager_index_groups()
             {
                 "name": "#{SPEC_NAME}",
                 "variables": ["#{sg.uuid.value}", "#{ROLE}"]
-            }   
+            }
         ,
             {
                 "name": "org",
                 "variables": ["#{sg.uuid.value}", "org"]
-            }   
-    
+            }
+
         ]
 
         |
@@ -78,7 +72,7 @@ def make_config()
         "eager_indexing_groups": [
                 [{"name": "clean", "variables": []}, {"name": "public", "variables" : []}],
                 [{"name": "clean", "variables": []}, {"name": "public", "variables" : []}, {"name": "readers", "variables" : []}],
-                
+
                 #{groups}
         ],
         "default_settings": #{File.read('./default_settings.json')},
