b62a7c6ffcabf70100f0086d48d49355875c2f32
felix <ruiz.felixruiz@gmail.com>
2022-10-28 12:31:45 +0200

    Update scripts

--- config/scripts/reset-elastic-cluster.sh
+++ config/scripts/reset-elastic-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "warning this will run queries on the triplestore and delete containers, you have 3 seconds to press ctrl+c"
 sleep 3
-docker-compose rm -fs elasticsearch search
+docker-compose rm -fs elasticsearch elasticsearch2 elasticsearch3 search
 sudo rm -rf data/elasticsearch/
 docker-compose exec -T virtuoso isql-v <<EOF
 SPARQL DELETE WHERE {   GRAPH <http://mu.semte.ch/authorization> {     ?s ?p ?o.   } };
