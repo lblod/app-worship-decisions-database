b4907f8ad77726c11197f78cd3af0da3a24bc470
benjay10 <ben.behaeghe@redpencil.io>
2024-03-21 16:33:06 +0100

    VDDS: add more config and improve config for speed
    Also add the config for the RemoteDataObjects and the FormData. The
    queries have been rewritten for better performance, while hopefully not
    giving up on correctness. This is a delicate balance.

--- config/vendor-data/subjectsAndPaths.js
+++ config/vendor-data/subjectsAndPaths.js
@@ -7,7 +7,21 @@ const HOSTNAME = envvar
   .asUrlString();
 
 export const subjects = [
-  //For Submissions via Consumer from Loket
+  /////////////////////////////////////////////////////////////////////////////
+  // Submissions
+  // via consumer from Loket
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  // The following queries always copy the whole model at once. E.g. if there
+  // is a Submission to be copied to the vendor graph, also copy the FormData
+  // and attachments to the vendor graph if they exist.
+  // All data is also deleted before copied again. This is to make sure changes
+  // are correctly updated. Otherwise you would see multiple values for the
+  // same predicate because the old values are never deleted.
+
+  /**
+   * Submission
+   */
   {
     type: 'http://rdf.myexperiment.org/ontologies/base/Submission',
     trigger: `
@@ -19,24 +33,180 @@ export const subjects = [
           pav:createdBy ?organisation ;
           pav:providedBy ?vendor .
       }
-      FILTER (REGEX(STR(?G), "^http://mu.semte.ch/graphs/organizations/"))
+      FILTER (REGEX(STR(?g), "^http://mu.semte.ch/graphs/organizations/"))
     `,
     remove: {
       delete: `
-        ?subject ?p ?o .
+        ?subject ?sp ?so .
+        ?formdata ?fp ?fo .
+        ?remotefile ?ro ?rp .
       `,
       where: `
-        ?subject ?p ?o .
+        {
+          ?subject <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?subject ?sp ?so .
+          ?formdata ?fp ?fo .
+        } UNION {
+          ?subject <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?formdata <http://purl.org/dc/terms/hasPart> ?remotefile .
+          ?remotefile ?ro ?rp .
+        }
       `,
     },
     copy: {
       insert: `
-        ?subject ?p ?o .
+        ?subject ?sp ?so .
+        ?formdata ?fp ?fo .
+        ?remotefile ?ro ?rp ;
+        ?remotefile <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#url> ?remotefileDownloadLink .
       `,
       where: `
-        ?subject ?p ?o .
+        {
+          ?subject <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?subject ?sp ?so .
+          ?formdata ?fp ?fo .
+        } UNION {
+          ?subject <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?formdata <http://purl.org/dc/terms/hasPart> ?remotefile .
+          ?remotefile <http://mu.semte.ch/vocabularies/core/uuid> ?remotefileUUID .
+          BIND (CONCAT("${HOSTNAME}files/", STR(?remotefileUUID), "/download") AS ?remotefileDownloadLink)
+          ?remotefile ?ro ?rp .
+        }
       `,
     },
   },
-  // Also for Form Data
+  /**
+   * FormData
+   *
+   * Linked to the Submission. Makes sure to check for a Submission to make a
+   * path to the Bestuurseenheid.
+   */
+  {
+    type: 'http://lblod.data.gift/vocabularies/automatische-melding/FormData',
+    trigger: `
+      ?subject a <http://lblod.data.gift/vocabularies/automatische-melding/FormData> .
+      ?submission
+        a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+        <http://www.w3.org/ns/prov#generated> ?subject .
+    `,
+    path: `
+      GRAPH ?g {
+        ?submission
+          a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+          <http://www.w3.org/ns/prov#generated> ?subject ;
+          pav:createdBy ?organisation ;
+          pav:providedBy ?vendor .
+      }
+      FILTER (REGEX(STR(?g), "^http://mu.semte.ch/graphs/organizations/"))
+    `,
+    remove: {
+      delete: `
+        ?submission ?sp ?so .
+        ?subject ?fp ?fo .
+        ?remotefile ?ro ?rp .
+      `,
+      where: `
+        {
+          ?submission <http://www.w3.org/ns/prov#generated> ?subject .
+          ?submission ?sp ?so .
+          ?subject ?fp ?fo .
+        } UNION {
+          ?submission <http://www.w3.org/ns/prov#generated> ?subject .
+          ?subject <http://purl.org/dc/terms/hasPart> ?remotefile .
+          ?remotefile ?ro ?rp .
+        }
+      `,
+    },
+    copy: {
+      insert: `
+        ?submission ?sp ?so .
+        ?subject ?fp ?fo .
+        ?remotefile ?ro ?rp .
+        ?remotefile <http://www.semanticdesktop.org/ontologies/2007/01/19/nie#url> ?remotefileDownloadLink .
+      `,
+      where: `
+        {
+          ?submission <http://www.w3.org/ns/prov#generated> ?subject .
+          ?submission ?sp ?so .
+          ?subject ?fp ?fo .
+        } UNION {
+          ?submission <http://www.w3.org/ns/prov#generated> ?subject .
+          ?subject <http://purl.org/dc/terms/hasPart> ?remotefile .
+          ?remotefile <http://mu.semte.ch/vocabularies/core/uuid> ?remotefileUUID .
+          BIND (CONCAT("${HOSTNAME}files/", STR(?remotefileUUID), "/download") AS ?remotefileDownloadLink)
+          ?remotefile ?ro ?rp .
+        }
+      `,
+    },
+  },
+  /**
+   * FileDataObjects ←→ RemoteDataObjects
+   *
+   * Attachments to the FormData of the Submission. Links to the Submission and
+   * FormData need to exist because a path to the Bestuurseenheid needs to be
+   * made.
+   */
+  {
+    type: 'http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject',
+    trigger: `
+      ?subject a <http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject> .
+      ?submission
+        a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+        <http://www.w3.org/ns/prov#generated> ?formdata .
+      ?formdata
+        a <http://lblod.data.gift/vocabularies/automatische-melding/FormData> ;
+        <http://purl.org/dc/terms/hasPart> ?subject .
+    `,
+    path: `
+      GRAPH ?g {
+        ?submission
+          a <http://rdf.myexperiment.org/ontologies/base/Submission> ;
+          <http://www.w3.org/ns/prov#generated> ?formdata ;
+          pav:createdBy ?organisation ;
+          pav:providedBy ?vendor .
+        ?formdata
+          a <http://lblod.data.gift/vocabularies/automatische-melding/FormData> ;
+          <http://purl.org/dc/terms/hasPart> ?subject .
+      }
+      FILTER (REGEX(STR(?g), "^http://mu.semte.ch/graphs/organizations/"))
+    `,
+    remove: {
+      delete: `
+        ?submission ?sp ?so .
+        ?formdata ?fp ?fo .
+        ?subject ?ro ?rp .
+      `,
+      where: `
+        {
+          ?submission <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?submission ?sp ?so .
+          ?formdata ?fp ?fo .
+        } UNION {
+          ?submission <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?formdata <http://purl.org/dc/terms/hasPart> ?subject .
+          ?subject ?ro ?rp .
+        }
+      `,
+    },
+    copy: {
+      insert: `
+        ?submission ?sp ?so .
+        ?formdata ?fp ?fo .
+        ?subject ?ro ?rp .
+      `,
+      where: `
+        {
+          ?submission <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?submission ?sp ?so .
+          ?formdata ?fp ?fo .
+        } UNION {
+          ?submission <http://www.w3.org/ns/prov#generated> ?formdata .
+          ?formdata <http://purl.org/dc/terms/hasPart> ?subject .
+          ?subject <http://mu.semte.ch/vocabularies/core/uuid> ?subjectUUID .
+          BIND (CONCAT("${HOSTNAME}files/", STR(?subjectUUID), "/download") AS ?subjectDownloadLink)
+          ?subject ?ro ?rp .
+        }
+      `,
+    },
+  },
 ];
